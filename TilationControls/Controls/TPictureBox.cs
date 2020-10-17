4ac18a709a05713958e37ef123b655cf683fb9a1
Gaspar <Gaspar@TABLET-AERO>
2020-10-16 22:09:24 -0300

    Agregado TPictureBox con configuracion de suavizado

---
+++ TilationControls/Controls/TPictureBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TilationControls.Controls
+{
+    class TPictureBox : PictureBox
+    {
+        private bool nnInterpolation;
+
+        public bool PixelPerfectInterpolation { get => nnInterpolation; set => nnInterpolation = value; }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            pe.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
+            pe.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+            base.OnPaint(pe);
+        }
+    }
+}
